3bd08998a8f9f9647a5cc73ce16f1536cc4845a7
Roman Katsala <rkatsala@gmail.com>
2017-10-26 15:24:26 +0300

    Add user model

---
+++ api/models/User.js
@@ -0,0 +1,137 @@
+/**
+ * User.js
+ *
+ * @description :: Kora ICO user model.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+/* global _ sails ValidationService */
+
+const bcrypt = require('bcrypt');
+
+const WLError = require('waterline/lib/waterline/error/WLError');
+
+const roles = {
+  admin: 'admin',
+  user: 'user'
+};
+const rolesList = _.values(roles);
+
+module.exports = {
+  constants: {
+    roles,
+    rolesList
+  },
+
+  attributes: {
+    phone: { type: 'string', required: true, phoneNumber: true },
+
+    userName: { type: 'string', required: true, alphanumericdashed: true },
+
+    userNameOrigin: { type: 'string' },
+
+    email: { type: 'string', email: true },
+
+    firstName: { type: 'string', required: true },
+
+    lastName: { type: 'string', required: true },
+
+    ethereumAddress: { type: 'string', ethereumAddress: true, required: true },
+
+    role: { type: 'string', in: rolesList, defaultsTo: roles.user },
+
+    encryptedPassword: { type: 'string' },
+
+    toJSON: function () {
+      var obj = this.toObject();
+
+      obj.userName = obj.userNameOrigin;
+      delete obj.userNameOrigin;
+      delete obj.encryptedPassword;
+
+      return obj;
+    }
+  },
+
+  types: {
+    phoneNumber: value => ValidationService.phoneNumber(value),
+    ethereumAddress: value => ValidationService.address(value)
+  },
+
+  indexes: [
+    {
+      attributes: { phone: 1 },
+      options: { unique: true }
+    }, {
+      attributes: { userName: 1 },
+      options: { unique: true }
+    }, {
+      attributes: { email: 1 },
+      options: { unique: true }
+    }
+  ],
+
+  beforeValidate: function (values, cb) {
+    if (values.userName) {
+      values.userNameOrigin = values.userName;
+      values.userName = values.userName.toLowerCase();
+    }
+
+    if (values.email) {
+      values.email = values.email.toLowerCase();
+    }
+
+    return cb();
+  },
+
+  beforeCreate: function (values, cb) {
+    let password = values.password;
+
+    delete values.password;
+
+    if (!password) {
+      if (sails.config.environment === 'development') {
+        // Password for development purposes
+        password = 'qwer1234';
+      } else {
+        return cb(new WLError({status: 400, reason: 'Password must be set'}));
+      }
+    }
+
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return cb(err);
+      }
+
+      bcrypt.hash(password, salt, function (err, hash) {
+        if (err) {
+          return cb(err);
+        }
+
+        values.encryptedPassword = hash;
+
+        return cb();
+      });
+    });
+  },
+
+  comparePassword: function (password, user, cb) {
+    bcrypt.compare(password, user.encryptedPassword, function (err, match) {
+      if (err) {
+        return cb(err);
+      }
+
+      return cb(null, match);
+    });
+  },
+
+  findOneUnique: function (identifier, cb) {
+    this.findOne({$or: [
+        {phone: identifier},
+        {userName: identifier.toLowerCase()},
+        {email: identifier.toLowerCase()}
+    ]})
+      .then(user => cb(null, user))
+      .catch(err => cb(err));
+  }
+};
