f42d14a82a3a2f4855d454d66a5c9c922b607675
Roman Katsala <rkatsala@gmail.com>
2017-10-26 14:51:26 +0300

    Add Validation service

---
+++ api/services/ValidationService.js
@@ -0,0 +1,14 @@
+/**
+ * ValidationService
+ * @description :: Set of validation functions
+ */
+
+const Web3Utils = require('web3-utils');
+
+module.exports = {
+  phoneNumber: value => /^[1-9]\d{9,12}$/i.test(value),
+
+  address: value => Web3Utils.isAddress(value),
+
+  hex: value => Web3Utils.isHex(value)
+};
