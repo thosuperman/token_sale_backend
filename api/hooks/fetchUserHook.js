f220d254d54ea2fac6936cf1c8218c93a9a9befb
Roman Katsala <rkatsala@gmail.com>
2017-11-03 17:37:46 +0200

    Add fetchUserHook

---
+++ api/hooks/fetchUserHook.js
@@ -0,0 +1,46 @@
+/**
+ * fetchUserHook
+ * @description :: Adds user to req if there are userId in session
+ * @return {[type]}       [description]
+ */
+
+/* global User */
+
+module.exports = function sessionTokenHook (sails) {
+  return {
+    // Add some routes to the app.
+    routes: {
+
+      // Add these routes _before_ anything defined in `config/routes.js`.
+      before: {
+
+        // Add a route that will match everything (using skipAssets to...skip assets!)
+        '/*': {
+          fn: function (req, res, next) {
+            const userId = req.session.userId;
+
+            if (!userId) {
+              return next();
+            }
+
+            User.findOne({id: userId}).exec(function (err, user) {
+              if (err) {
+                return res.negotiate(err);
+              }
+
+              if (!user) {
+                return res.json(401, {message: 'Could not find user in session!'});
+              }
+
+              req.user = user;
+
+              return next();
+            });
+          },
+
+          skipAssets: true
+        }
+      }
+    }
+  };
+};
