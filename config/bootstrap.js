bc3254fa7c6044e76bc5d67aa077cdddd203cfc8
Roman Katsala <rkatsala@gmail.com>
2017-12-11 12:02:06 +0200

    Add node-schedule and test for pm2

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,9 +11,22 @@
 
 /* global sails */
 
+const schedule = require('node-schedule');
+
 module.exports.bootstrap = function (cb) {
   // Option for req.ip
   sails.hooks.http.app.set('trust proxy', true);
+
+  if (process.env.NODE_APP_INSTANCE === 0) {
+    let j = schedule.scheduleJob('*/5 * * * * *', function () {
+      sails.log.info(`[${process.env.NODE_APP_INSTANCE}]:`, 'The answer to life, the universe, and everything!');
+    });
+  }
+
+  let j2 = schedule.scheduleJob('*/7 * * * * *', function () {
+    sails.log.info(`[${process.env.NODE_APP_INSTANCE}]:`, 'The answer to life, the universe, and everything!');
+  });
+
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
