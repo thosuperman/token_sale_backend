c5ed3ab863dec0feea00fda0ef9371882fb041eb
Roman Katsala <rkatsala@gmail.com>
2017-12-11 12:32:35 +0200

    Add basic config for scheduler

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,22 +11,22 @@
 
 /* global sails */
 
-const schedule = require('node-schedule');
+const scheduler = require('node-schedule');
 
 module.exports.bootstrap = function (cb) {
   // Option for req.ip
   sails.hooks.http.app.set('trust proxy', true);
 
-  if (process.env.NODE_APP_INSTANCE === 0) {
-    let j = schedule.scheduleJob('*/5 * * * * *', function () {
-      sails.log.info(`[${process.env.NODE_APP_INSTANCE}]:`, 'The answer to life, the universe, and everything!');
+  if (
+    // Run job in development environment
+    process.env.NODE_ENV === 'development' || // TODO: Remove after task will be done
+    // Run job only for one instance of PM2
+    process.env.NODE_APP_INSTANCE == 0 // eslint-disable-line eqeqeq
+  ) {
+    scheduler.scheduleJob('*/10 * * * * *', function () {
+      sails.log.info(`[${process.env.NODE_APP_INSTANCE}]`, 'The answer to life, the universe, and everything!');
     });
   }
-
-  let j2 = schedule.scheduleJob('*/7 * * * * *', function () {
-    sails.log.info(`[${process.env.NODE_APP_INSTANCE}]:`, 'The answer to life, the universe, and everything!');
-  });
-
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
