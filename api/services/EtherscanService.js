cdf9c8deb44a577fca2cef98d080b0fbd5826be6
Roman Katsala <rkatsala@gmail.com>
2017-12-12 19:47:17 +0200

    Update EtherscanService

--- api/services/EtherscanService.js
+++ api/services/EtherscanService.js
@@ -8,11 +8,18 @@
 
 // TODO; Remove 'rinkeby' after all logic implementation
 const api = require('etherscan-api').init(sails.config.etherscanApiKey, 'rinkeby');
+const koraWallet = sails.config.koraEtherWallet;
 
 module.exports = {
-  txlist: function ({address, startblock, endblock, sort}, cb) {
-    let promise = api.account.txlist(address, startblock, endblock, sort);
+  txlist,
 
-    return MiscService.cbify(promise, cb);
+  koraWalletTxlist: function ({startblock, endblock, sort}, cb) {
+    return txlist({address: koraWallet, startblock, endblock, sort}, cb);
   }
 };
+
+function txlist ({address, startblock, endblock, sort}, cb) {
+  let promise = api.account.txlist(address, startblock, endblock, sort);
+
+  return MiscService.cbify(promise, cb);
+}
