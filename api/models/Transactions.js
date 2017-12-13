c5f5b5440506267c9c306557df96cdeb0a686cac
Roman Katsala <rkatsala@gmail.com>
2017-12-13 14:46:12 +0200

    Add calculaate KTN and find from user logic to Transactions model

--- api/models/Transactions.js
+++ api/models/Transactions.js
@@ -5,7 +5,9 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
-/* global _ MiscService */
+/* global sails _ MiscService User ExchangeRates */
+
+const Web3Utils = require('web3-utils');
 
 const types = {
   BTC: 'BTC',
@@ -24,12 +26,61 @@ module.exports = {
 
     raw: { type: 'json', required: true },
 
-    date: { type: 'date', required: true }
+    date: { type: 'date', required: true },
+
+    value: { type: 'float' },
+
+    KTN: { type: 'float' },
+
+    from: { model: 'user' }
   },
 
   beforeValidate: function (values, cb) {
-    if (values.raw && values.raw.timeStamp) {
-      values.date = new Date(values.raw.timeStamp * 1000);
+    if (values.raw) {
+      if (values.raw.timeStamp) {
+        values.date = new Date(values.raw.timeStamp * 1000);
+      }
+
+      let promises = [];
+
+      if (values.raw.value) {
+        values.value = Web3Utils.fromWei(values.raw.value, 'ether');
+
+        promises.push(
+          ExchangeRates.findOne({
+            where: {
+              date: {'<=': values.date || new Date()}
+            },
+            sort: 'date DESC'
+          })
+          .then(record => {
+            if (record) {
+              // TODO: Сhange koraExchangeRate logic
+              values.KTN = values.value * record.USD * sails.config.koraExchangeRate;
+            }
+          })
+        );
+      }
+
+      if (values.raw.from) {
+        promises.push(
+          User.findOne({sendingEthereumAddress: values.raw.from})
+            .then(user => {
+              if (user) {
+                values.from = user.id;
+              }
+            })
+        );
+      }
+
+      if (promises.length) {
+        return Promise.all(promises)
+          .then(() => cb())
+          .catch(err => {
+            sails.log.error(err);
+            return cb();
+          });
+      }
     }
 
     return cb();
