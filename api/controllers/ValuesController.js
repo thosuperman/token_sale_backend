34e0e6cf526c566eb5f90e5845fa0e04860d9159
Roman Katsala <rkatsala@gmail.com>
2017-12-14 19:38:51 +0200

    Add wallets and exchangeRates to ValuesController

--- api/controllers/ValuesController.js
+++ api/controllers/ValuesController.js
@@ -5,7 +5,9 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-/* global Transactions ExchangeRates MiscService */
+/* global Transactions ExchangeRates MiscService KoraService */
+
+const qrcode = require('qrcode');
 
 module.exports = {
 
@@ -15,14 +17,18 @@ module.exports = {
   index: function (req, res) {
     Promise.all([
       calcUserKNTBalance({userId: req.user.id}),
-      ExchangeRates.findLastByTypes()
+      ExchangeRates.findLastByTypes(),
+      KoraService.exchangeRates(),
+      fetchKoraWallets()
     ])
-      .then(([KNTBalance, exchangeRates]) => {
+      .then(([KNTBalance, exchangeRates, koraExchangeRates, wallets]) => {
         let result = exchangeRates.reduce((previous, {type, USD}) => {
           previous[type + '_USD'] = USD;
 
           return previous;
         }, {KNTBalance});
+
+        Object.assign(result, koraExchangeRates, wallets);
 
         return result;
       })
@@ -44,6 +50,42 @@ function calcUserKNTBalance ({userId}, cb) {
       }, 0);
     })
     .then(sum => +sum.toFixed(10));
+
+  return MiscService.cbify(promise, cb);
+}
+
+function fetchKoraWallets (cb) {
+  let cache = {
+    BTCWallet: {},
+    ETHWallet: {}
+  };
+
+  let promise = KoraService.wallets()
+    .then(({BTC, ETH}) => {
+      cache.BTCWallet.address = BTC;
+      cache.ETHWallet.address = ETH;
+
+      return Promise.all([BTC, ETH].map(a => generageQRCode(a)));
+    })
+    .then(([BTC, ETH]) => {
+      cache.BTCWallet.qrcode = BTC;
+      cache.ETHWallet.qrcode = ETH;
+
+      return cache;
+    });
+
+  return MiscService.cbify(promise, cb);
+}
+
+function generageQRCode (text, cb) {
+  let promise = new Promise((resolve, reject) =>
+    qrcode.toDataURL(text, (err, url) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(url);
+    }));
 
   return MiscService.cbify(promise, cb);
 }
