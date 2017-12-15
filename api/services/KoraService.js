4bade1b3bc1552ba3c60be16e363f9dcde2693ce
Roman Katsala <rkatsala@gmail.com>
2017-12-15 19:05:20 +0200

    Add pre-sale info to KoraService

--- api/services/KoraService.js
+++ api/services/KoraService.js
@@ -5,6 +5,28 @@
 
 /* global sails MiscService */
 
+const USD_KNT = 10;
+
+const preSaleRaw = [
+  {discount: 90, amountUSD: 500000, note: 'Reserved for core team and early advisors.'},
+  {discount: 65, amountUSD: 500000, note: 'Reserved for early advisors and Contributors.'},
+  {discount: 55, amountUSD: 500000, note: 'Restricted to HNW, VC and accredited Contributors if in USA.'},
+  {discount: 50, amountUSD: 500000, note: 'Restricted to HNW, VC and accredited Contributors if in USA.'},
+  {discount: 45, amountUSD: 1000000, note: 'Restricted to HNW, VC and accredited Contributors if in USA.'},
+  {discount: 40, amountUSD: 1000000, note: 'Restricted to early members of community.'}
+];
+
+const preSale = preSaleRaw.map((s, i, arr) => {
+  s.USD_KNT = +(USD_KNT * 100 / (100 - s.discount)).toFixed(10);
+  s.KNT_USD = +(1 / s.USD_KNT).toFixed(10);
+  s.amountKNT = +(s.USD_KNT * s.amountUSD).toFixed(10);
+  s.fullAmountUSD = i === 0 ? s.amountUSD : +(arr[i - 1].fullAmountUSD + s.amountUSD).toFixed(10);
+
+  return s;
+});
+
+sails.log.info('Pre-sale info:', preSale);
+
 module.exports = {
   wallets: function (cb) {
     let promise = Promise.resolve({
